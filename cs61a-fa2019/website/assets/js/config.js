237adb56335d6d3f4bcfc7924a7a44a6cfc5c284
Joe Mo <joe.mo@berkeley.edu>
2020-08-09 04:11:17 -0700

    Updated files

---
+++ cs61a-fa2019/website/assets/js/config.js
@@ -0,0 +1,8 @@
+/* Start/end date of semester. Make sure to change utils.py as well */
+var startDate = '2019-08-28', endDate = '2019-12-20'; 
+
+/* Calendar Specification */
+var officeHoursCalendarId = 'berkeley.edu_trbr43nt1b3t929mefg78kd57c@group.calendar.google.com';
+var instructorOfficeHoursCalendarId = 'berkeley.edu_35kcpi7uv9jstvkqtjeeo6a4g8@group.calendar.google.com';
+var lecturesCalendarId = 'berkeley.edu_nbg9eq84jciksa4303u4if57bg@group.calendar.google.com';
+var sectionsCalendarId = 'berkeley.edu_0hkdsstmtih0qgp5j0ganu5qc0@group.calendar.google.com';
